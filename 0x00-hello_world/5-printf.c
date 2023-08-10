51aac4c907414a428a61ba002cf16681f58c764c
DaisyAram <daisyarama03@gmail.com>
2023-08-10 06:40:31 -0700

    Hello printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * Decription: main - Prints out a stdout.
+ * Return: 0 if success.
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
