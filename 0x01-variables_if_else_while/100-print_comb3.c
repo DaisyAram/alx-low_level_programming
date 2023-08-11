019dd55c6c2c6a0a46126b71ee4da6c2a271a4f1
DaisyAram <daisyarama03@gmail.com>
2023-08-11 04:56:14 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Prints all possible combinations of two digits
+ * Return: Always 0
+ */
+
+int main(void)
+
+{
+	int d, p;
+
+	for (d = '0' ; d < '9' ; d++)
+	{
+	for (p = d + 1; p <= '9'; p++)
+	{
+	if (p != d)
+	{
+	putchar(d);
+	putchar(p);
+	if (d == 'g' && p == '9')
+	continue;
+	putchar(',');
+	putchar(' ');
+	}
+	}
+	}
+	putchar('\n');
+	return (0);
+}
