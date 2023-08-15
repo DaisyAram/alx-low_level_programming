e709c8560e65c188dd32d2296fe8e43c8eb7f31b
DaisyAram <daisyarama03@gmail.com>
2023-08-15 12:29:11 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/5-main.c
+++ 0x02-functions_nested_loops/5-main.c
@@ -6,8 +6,7 @@
  * Return: Always 0.
  */
 int main(void)
-{
-    int r;
+{   int r;
 
     r = print_sign(98);
 	_putchar(',');
--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,7 +2,7 @@
 
 /**
  * print_sign - Prints the sign of a number
- *@n: The number to be checked
+ * @n: The number to be checked
  * Return: 1 for postive num, -1 for negative num or zero for anything else
  */
 int print_sign(int n);
