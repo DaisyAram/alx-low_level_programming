5646e3198ecf682270ed9a51dc9daf2ff9c11679
DaisyAram <daisyarama03@gmail.com>
2023-08-15 06:47:41 -0700

    Print 10 times alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alphabet _x10 - Make alphabet x10 times
+ * Return: 0 success
+ */
+
+void print_alphabet_x10(void)
+{
+	char c;
+	int i = 0;
+
+	while (i <= 9)
+	{
+	for (c = 'a'; c <= 'z'; c++)
+	{
+	_putchar(c);
+	}
+	_putchar ('\n');
+	i++;
+	}
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -2,7 +2,7 @@ int _putchar(char);
 
 void print_alphabet(void);
 
-void print_alphabet_x10(void);
+void print_alphabet_x10();
 
 int _islower(int c);
 
