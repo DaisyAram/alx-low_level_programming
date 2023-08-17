c7fdd88151d87cdc0c2dbb041d4ff6d4f61f4768
DaisyAram <daisyarama03@gmail.com>
2023-08-17 08:09:52 -0700

    8-print_square.c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -6,27 +6,25 @@
  */
 void print_diagonal(int n)
 {
-        if (n <= 0)
-        {
-        _putchar('\n');
-        }
-        else
-        {
-        int i, j;
+	if (n <= 0)
+	{
+	_putchar('\n');
+	}
+	else
+	{
+	int i, j;
 
-        for (i = 0; i < n; i++)
-        {
-        for (j = 0; j < n; j++)
-        {
-        if (j == i)
-        _putchar('\\');
-        else if (j < i)
+	for (i = 0; i < n; i++)
+	{
+	for (j = 0; j < n; j++)
+	{
+	if (j == i)
+	_putchar('\\');
+	else if (j < i)
 
-        _putchar(' ');
-        }
-        _putchar('\n');
-        }
-        }
+	_putchar(' ');
+	}
+	_putchar('\n');
+	}
+	}
 }
-~
-
--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -7,22 +7,21 @@
 
 void print_square(int size)
 {
-        if (size <= 0)
-        {
-        _putchar('\n');
-        }
-        else
-        {
-        int i, j;
+	if (size <= 0)
+	{
+	_putchar('\n');
+	}
+	else
+	{
+	int i, j;
 
-        for (i = 0; i < size; i++)
-        {
-        for (j = 0; j < size; j++)
-        {
-        _putchar('#');
-        }
-        _putchar('\n');
-        }
-        }
+	for (i = 0; i < size; i++)
+	{
+	for (j = 0; j < size; j++)
+	{
+	_putchar('#');
+	}
+	_putchar('\n');
+	}
+	}
 }
-
