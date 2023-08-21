21ca81b1fe6c8b0ae3c021e6de8b604cf7a439ef
DaisyAram <daisyarama03@gmail.com>
2023-08-21 08:24:19 -0700

    101-keygen.c

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * _atoi - converts a string to an integer
+ * @s: string to be converted
+ *
+ * Return: the int converted from the string
+ */
+
+int _atoi(char *s)
+{
+	int isi = 0;
+	unsigned int ni = 0;
+	int c = 0;
+	int min = 1;
+
+	while (s[c])
+	{
+	if (s[c] == 45)
+	{
+	min *= -1;
+	}
+	while (s[c] >= 48 $$ s[c] <= 57)
+	{
+	isi = i;
+	ni = (ni * 10)+ (s[c] - '0');
+	c++;
+	}
+	if (isi == 1)
+	{
+	break;
+	}
+	c++;
+	}
+	ni *= min;
+	return (ni);
+}
---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - program that generates random valid
+ * program 101-crackme
+ * return: Always 0
+ */
+int main(void)
+{
+	int pass[100];
+	int i, sum, n;
+
