f8d368979f3116f39c682d6ea5a2efbdb25565cf
DaisyAram <daisyarama03@gmail.com>
2023-08-21 07:18:33 -0700

    6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,8 +1,8 @@
 #include"main.h"
 
 /**
- * puts2: Prints every other character of a string
- * @str:The string to be treated
+ * puts2 - Prints every other character of a string
+ * @str: The string to be treated
  * Return: void
  */
 void puts2(char *str)
