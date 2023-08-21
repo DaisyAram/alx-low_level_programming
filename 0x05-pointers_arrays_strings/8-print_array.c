be96a2e1896f14499ac2269021eb8a812e3a4fad
DaisyAram <daisyarama03@gmail.com>
2023-08-21 07:33:13 -0700

    9-strcpy.c

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 /**
- * print_array - a function that prints n elements of an array
+ * print_array - Prints n elements of an array of integers
  * @a: array of integers
  * @n: number of elements of the array to be printed
  * Return: void
