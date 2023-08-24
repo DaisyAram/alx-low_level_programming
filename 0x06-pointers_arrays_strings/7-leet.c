37c7a269ec1a4b6918b54ff435243ed3baed75b0
DaisyAram <daisyarama03@gmail.com>
2023-08-23 23:51:47 -0700

    leet

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,6 +1,28 @@
 #include "main.h"
 /**
- * leet - encodes to 1337
+ * leet - encodes to 1337speak
+ * @i: input value
+ * Return: nivalue
  */
-char *leet(char *)
+char *leet(char *i)
+{
+	int a;
+
+	int b;
+
+	char s1[] = "aAeEoOtTlL";
+	char s2[] = "4433007711";
+
+	for (a = 0; i[a] != '\0'; a++)
+	{
+	for (b = 0; b < 10; b++)
+	{
+	if (i[a] == s1[b])
+	{
+	i[a] = s2[b];
+	}
+	}
+	}
+	return (i);
+}
 
