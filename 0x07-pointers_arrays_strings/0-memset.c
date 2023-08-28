6880b36064df184a692c884cb1003ad2cd9ab9c0
DaisyAram <daisyarama03@gmail.com>
2023-08-28 05:24:37 -0700

    Set string

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _memset  fills memory with a constant byte
+ * _memset - fills memory with a constant byte
  * @s: starting address of memort to be filled
  * @b: value
  * @n: number of bytes to be changed
