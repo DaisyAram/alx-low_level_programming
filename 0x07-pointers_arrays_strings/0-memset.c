b12325b36427ac94ade0a3a33ceb4e4bd35ab04f
DaisyAram <daisyarama03@gmail.com>
2023-08-28 03:35:50 -0700

    memset

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * _memset  fills memory with a constant byte
- * @s: starting address
+ * @s: starting address of memort to be filled
  * @b: value
  * @n: number of bytes to be changed
  * Return: changed array with new value for n bytes
