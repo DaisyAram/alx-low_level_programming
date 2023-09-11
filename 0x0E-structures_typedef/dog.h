5f173fc5ab0f7ea46b2449523cb6100b29629f0e
DaisyAram <daisyarama03@gmail.com>
2023-09-11 07:24:45 -0700

    struct

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -21,16 +21,14 @@ struct dog
 
 
 /**
- * dog_t - typedef for struct dog
+ * dog_t - Typedef for struct dog
  */
-
 typedef struct dog dog_t;
 
-int _strlen(char *s);
-void print_dog(struct dog *d);
-dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
 char *_strcpy(char *dest, char *src);
+int _strlen(char *s);
 void init_dog(struct dog *d, char *name, float age, char *owner);
-
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
 #endif
