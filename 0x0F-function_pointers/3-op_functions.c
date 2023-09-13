087dbf1cb6b4351b500922d70731fc26ddfbf0f5
DaisyAram <daisyarama03@gmail.com>
2023-09-13 05:33:08 -0700

    funptr

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include "3-calc.h"
+
+/**
+ * get_op_func - chooses function to perform
+ *@s: operator passed as argument to the program
+ * Return: pointer to the function that corresponds
+ * to the operator given as a parameter
+ */
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {
+	{"+", op_add},
+	{"-", op_sub},
+	{"*", op_mul},
+	{"/", op_div},
+	{"%", op_mod},
+	{NULL, NULL}
+	};
+
+	int i = 0;
+
+	while (ops[i].op != NULL && *(ops[i].op) != *s)
+	i++;
+	return (ops[i].f);
+}
