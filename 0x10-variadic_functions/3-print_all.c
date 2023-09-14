c859af69fbaa1f5d405223addbc2e9748f0fdc4a
DaisyAram <daisyarama03@gmail.com>
2023-09-14 05:00:41 -0700

    variadic pointers

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -11,46 +11,40 @@
 void print_all(const char * const format, ...)
 {
 	int j = 0;
-	char *s;
+	char *s, *sep = "";
 
 	va_list list;
 
 	va_start(list, format);
-
 	if (format)
 	{
-	while(format[j])
+	while (format[j])
 	{
 	switch (format[j])
-
 	{
-
 	case 's':
 	s = va_arg(list, char*);
 	if (!s)
 	s = "(nil)";
-	printf("%s", s);
+	printf("%s%s", sep, s);
 	break;
-
 	case 'c':
-	printf("%c", va_arg(list, int));
+	printf("%s%c", sep, va_arg(list, int));
 	break;
-
 	case 'i':
-	printf("%d", va_arg(list, int));
+	printf("%s%d", sep, va_arg(list, int));
 	break;
-
 	case 'f':
-	printf("%d", va_arg(list, int));
+	printf("%s%d", sep, va_arg(list, int));
 	break;
 	default:
 	j++;
 	continue;
 	}
+	sep = ", ";
 	j++;
 	}
 	}
 	printf("\n");
 	va_end(list);
-
 }
