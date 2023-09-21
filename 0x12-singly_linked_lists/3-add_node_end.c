7904923b2331426f6d5ed695516b3f596531ab74
DaisyAram <daisyarama03@gmail.com>
2023-09-21 12:40:31 -0700

    linked list

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,1 +1,41 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <string.h>
+/**
+ * add_node_end - adds a new node at the end of a list_t list
+ * @head: double pointer that points to list_t
+ * @str: string to put new node
+ * Return: address of the new element
+ */
+
 list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *new_node;
+	list_t *temp = *head;
+	unsigned int len = 0;
+
+	while (str[len])
+	len++;
+
+	/** memory allocation */
+	new_node = malloc(sizeof(list_t));
+	/** if node is null then failure */
+	if (!new_node)
+	return (NULL);
+	else
+	/** copy string into a new node */
+	new_node->str = strdup(str);
+	/** update the length of the string */
+	new_node->len = len;
+	/** update the head of the list */
+	new_node->next = NULL;
+	if (*head == NULL)
+	{
+	(*head) = new_node;
+	return (new_node);
+	}
+	while (temp->next)
+	temp = temp->next;
+	temp->next = new_node;
+	return (new_node);
+}
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -25,4 +25,8 @@ size_t list_len(const list_t *h);
 void free_list(list_t *head);
 
 list_t *add_node(list_t **head, const char *str);
+
+list_t *add_node_end(list_t **head, const char *str);
+
+void free_list(list_t *head);
 #endif
