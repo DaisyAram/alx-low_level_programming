38f6c43b1721ad48a220d7982a9b8abdf2116f64
DaisyAram <daisyarama03@gmail.com>
2023-09-22 07:03:04 -0700

    lists

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -3,17 +3,19 @@
 
 /**
  * free_list - frees a list_t list.
+ * @head: head of the node
  * Return: nothing
  */
 void free_list(list_t *head)
 {
-	list_t *temp;
+	list_t *temp; /** declare  a pointer to a variable */
 
-	while (head)
+	while (head != NULL) /** until the list is empty */
 	{
-	temp = head->next;
-	free(head->str);
-	free(head);
-	free (temp);
+	temp = head; /** assign node to temp */
+	temp = head->next;/** traverse through the list */
+	free(head->str) ;/** free the list */
+	free(head); /** free memory allocated for  temp */
+	head = temp;
 	}
 }
