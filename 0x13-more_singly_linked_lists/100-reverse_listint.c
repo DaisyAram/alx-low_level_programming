afc2f3a579cbd419da53e5fb2138ae20c854e0a3
DaisyAram <daisyarama03@gmail.com>
2023-09-26 03:11:31 -0700

    linked lists

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * reverse_listint - reverses a listint_t linked list
+ * @head : head
+ *
+ * Return: first node of the reversed list
+ */
+
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t *next = NULL;
+	listint_t *prev = NULL;
+
+	while (*head)
+	{
+	next = (*head)->next;
+	(*head)->next = prev;
+	prev = *head;
+	*head = next;
+	}
+	*head = prev;
+	return (*head);
+}
---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,44 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * insert_nodeint_at_index - inserts a new node at a given position
+ * @head: head
+ * @idx: the index of the list where the new node should be added
+ * @n: integer
+ * Return: the address of the new node, or NULL if it failed
+ */
+
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	listint_t *new;
+	listint_t *temp;
+	unsigned int a;
+
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+	return (NULL);
+
+	new->n = n;
+	new->next = NULL;
+
+	if (idx == 0)
+	{
+	new->next = *head;
+	*head = new;
+	}
+	return (new);
+	
+	for (a = 0; a < idx - 1 && temp != NULL; a++)
+	temp = temp->next;
+	
+	if (temp == NULL)
+	return (NULL);
+	
+	else
+	{
+	new->next = temp->next;
+	temp->next = new;
+	return (new);
+	}
+}
