7aaa63b910038cc70917e6f374611de4604edc6d
DaisyAram <daisyarama03@gmail.com>
2023-09-29 00:33:40 -0700

    corrections

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -10,5 +10,20 @@
 void print_binary(unsigned long int n)
 {
 	unsigned long int a;
-	int j;
-	count++;
+	int j, count = 0;
+
+	for (j = 64; j >= 0; j--)
+	{
+	a = n >> 1;
+	if (a & 1)
+	{
+	_putchar('1');
+	count ++;
+	}
+
+	else if (count)
+	_putchar('0');
+	}
+	if (!count)
+	_putchar('0');
+}
