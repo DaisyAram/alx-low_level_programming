28265946b5c0ec1cbdbae7a81bdb4e588d5da240
DaisyAram <daisyarama03@gmail.com>
2023-10-03 01:50:18 -0700

    corrections

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -14,4 +14,10 @@ int create_file(const char *filename, char *text_content);
 
 int append_text_to_file(const char *filename, char *text_content);
 
+int main(int argc, char *argv[]);
+
+char *define_buffer(char *file);
+
+void close_file(int file);
+
 #endif
