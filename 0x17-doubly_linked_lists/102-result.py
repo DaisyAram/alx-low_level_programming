79103588b19cd7de4d3eb4dee0e34c6bc9f11c1e
DaisyAram <daisyarama03@gmail.com>
2023-12-07 09:08:20 -0800

    palindrome

--- 0x17-doubly_linked_lists/102-result.py
+++ 0x17-doubly_linked_lists/102-result.py
@@ -6,6 +6,3 @@ for i in range(100, 1000):
         product = i * j
         if str(product) == str(product)[::-1] and product > largest_palindrome:
             largest_palindrome = product
-# Save the result in the file
-with open("102-result", "w") as file:
-    file.write(str(largest_palindrome))
