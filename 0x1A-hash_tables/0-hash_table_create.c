b632db36bae59babc25e41667a4b6ef5d0e3d50d
DaisyAram <daisyarama03@gmail.com>
2023-12-22 00:42:19 -0800

    0. >>> ht = {}

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -10,20 +10,20 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table_t *table = NULL;
 
 	if (size == 0)
-	return NULL;
+	return (NULL);
 
 	table = malloc(sizeof(hash_table_t));
 	if (table == NULL)
-	return NULL;
+	return (NULL);
 
 	table->size = size;
-	table->array = calloc(size, sizeof(hash_node_t*));
+	table->array = calloc(size, sizeof(hash_node_t *));
 
 	if (table->array == NULL)
 	{
 	free(table);
-	return NULL;
+	return (NULL);
 	}
 
-	return table;
+	return (table);
 }
