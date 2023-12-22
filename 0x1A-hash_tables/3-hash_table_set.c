516db0bf133934df77e5207529a752008121312b
DaisyAram <daisyarama03@gmail.com>
2023-12-22 01:01:58 -0800

    3. >>> ht['betty'] = 'cool'

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -3,6 +3,7 @@
  * hash_table_set -  adds an element to the hash table.
  * @key: key
  * @value: valiue associated with the key
+ * @ht: hash table
  *
  * Return: 1 on success, otherwise 0
  */
@@ -11,9 +12,6 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	unsigned long int index = key_index((const unsigned char *)key, ht->size);
 	hash_node_t **node = &ht->array[index];
 
-	/*char *valuecopy, *keycopy;
-	unsigned long int index = 0;
-	hash_node_t *bucket, *new_node;*/
 	if (!ht || !key || !*key || !value)
 	return (0);
 
