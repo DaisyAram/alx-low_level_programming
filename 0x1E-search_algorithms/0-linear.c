633d5e0d7f95b1ba8e7f195dba2008d3a9cf0541
DaisyAram <daisyarama03@gmail.com>
2024-04-08 10:21:45 -0700

    mandatory

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -14,17 +14,15 @@ int linear_search(int *array, size_t size, int value)
 	size_t i;
 
 	if (array == NULL)
-		return (-1);
-	for (i = 0; i < size; i++)/* iterate through the element in the array*/
-	{
-	 printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+	return (-1);
+
+	for (i = 0; i < size; i++)
+	printf("Value checked array[%ld] = [%d]\n", i, array[i]);
 	/*if current element matchesthe target return index*/
 	if (array[i] == value)
 	{
 		return (i);
 	}
-	}
-
 	/* if target is not found return -1*/
 	return (-1);
 }
--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -8,55 +8,35 @@
  * @size: number of elements in array
  * @value: is the value to search for
  *
- * Return: index else -1 for fail 
+ * Return: mid index else -1 for fail 
  */
 int binary_search(int *array, size_t size, int value)
 {
-	size_t low = 0, high = size - 1, mid;
+	size_t low = 0, high = size -1, mid;
 
-	if (array == NULL)/*null array*/
-	return (-1);
+	if (array == NULL)
+		return (-1);
 
-	printf("Searching in array: ");
-	for (low = 0; low < size; low++)/*iterate on lower segment*/
+	while (low <= high)
 	{
-	if (low > 0)
-	printf(", ");
-        printf("%d, ", array[low]);
-	}
-	printf("\n");
-
-	while (low <= high)/*check for mid value*/
-	{
-	mid = low + (high - low) / 2;
-	if (array[mid] == value)/*if mid is equal to value*/
-	{
-	printf("Found %d at index: %ld\n\n", value, mid);
-	return (mid);
-	}
+		mid = ((low + high)/ 2);
+		if (array[mid] == value)
+		{
+			return (mid);
+		}
+		else if (array[mid] > value)
+		{
+			high = mid -  1;
+		printf("Searching in subarray: [%d, ", array[low]);
+		printf("%d]\n", array[high]);
+		}
+		else
+		{
+			low = mid + 1;
+			printf("Searching in array: [%d, ", array[low]);
+			printf("%d]\n", array[high]);
+		}
 	
-	if (array[mid] < value)
-	{
-	low = mid + 1;
-	printf("Searching in array: [%d, ", array[low]);
-	printf("%d]\n", array[high]);
-        }
-
-	else
-        {
-	high = mid - 1;
-	printf("Searching in subarray: [%d, ", array[low]);
-	printf("%d]\n", array[high]);
-        }
-	if (low > high) /*low is greater than high, the value is not in the array */
-	{
-	printf("Value checked array[%ld] = [%d]\n", high, array[high]);
-	printf("Value checked array[%ld] = [%d]\n", low, array[low]);
-	printf("Value %d not present in array\n", value);
-	return (-1);
-        }
-
-	printf("Searching in array: [%d, ", array[low]);
-	printf("%d]\n", array[high]);
 	}
+	return (-1);
 }
