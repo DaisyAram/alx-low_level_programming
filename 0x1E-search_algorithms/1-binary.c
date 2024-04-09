508cd69cfa3beab0f712f6c527c330a6b9543de0
DaisyAram <daisyarama03@gmail.com>
2024-04-09 10:40:57 -0700

    betty

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -8,19 +8,30 @@
  * @size: number of elements in array
  * @value: is the value to search for
  *
- * Return: mid index else -1 for fail 
+ * Return: mid index else -1 for fail
  */
 int binary_search(int *array, size_t size, int value)
 {
-	size_t low = 0, high = size -1, mid;
+	size_t low = 0, high = size - 1, mid, i;
 
 	if (array == NULL)
 		return (-1);
 
 	while (low <= high)
 	{
-		mid = ((low + high)/ 2);
-		 printf("Searching in array: ");
+		mid = ((low + high) / 2);
+		printf("Searching in array:");
+
+		for (i = low; i <= high; i++)
+		{
+			if (i != low)
+			{
+				printf(", ");
+			}
+			printf("%d", array[i]);
+		}
+		printf("\n");
+
 		if (array[mid] == value)
 		{
 			return (mid);
@@ -33,7 +44,6 @@ int binary_search(int *array, size_t size, int value)
 		{
 			low = mid + 1;
 		}
-	
 	}
 	return (-1);
 }
