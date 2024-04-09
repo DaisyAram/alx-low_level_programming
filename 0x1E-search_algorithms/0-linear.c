90b4a98518fdcff4df1ac4d36551220ad33a647d
DaisyAram <daisyarama03@gmail.com>
2024-04-09 05:42:24 -0700

    linear

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -13,16 +13,16 @@ int linear_search(int *array, size_t size, int value)
 {
 	size_t i;
 
-	if (array == NULL)
+	if (array == NULL || size == 0)
 	return (-1);
 
-	for (i = 0; i < size; i++)
-	printf("Value checked array[%ld] = [%d]\n", i, array[i]);
-	/*if current element matchesthe target return index*/
-	if (array[i] == value)
+	for (i = 0; i  < size; i++)
 	{
-		return (i);
+		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+		if (array[i] == value)
+		{
+			return (i);
+		}
 	}
-	/* if target is not found return -1*/
 	return (-1);
 }
