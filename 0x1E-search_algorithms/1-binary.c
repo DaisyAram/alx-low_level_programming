b71403146e6e5ee913d4aab4152e13baef1a36bf
DaisyAram <daisyarama03@gmail.com>
2024-04-09 10:43:07 -0700

    betty

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -20,7 +20,7 @@ int binary_search(int *array, size_t size, int value)
 	while (low <= high)
 	{
 		mid = ((low + high) / 2);
-		printf("Searching in array:");
+		printf("Searching in array: ");
 
 		for (i = low; i <= high; i++)
 		{
